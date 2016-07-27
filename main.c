c1ec1f23863d9fe9ed92acb0b68eea23831d3e4b
Maxime Bedard <maxime.bedard@shopify.com>
2016-07-27 19:23:29 -0400

    yolo

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
@@ -95,7 +97,6 @@ void heat_dissipation_par(int m, int n, float matrix[2][m][n], int np, float td,
   cl_device_id device_id         = NULL;
   cl_context context             = NULL;
   cl_command_queue command_queue = NULL;
-  cl_mem memobj                  = NULL;
   cl_program program             = NULL;
   cl_kernel kernel               = NULL;
   cl_platform_id platform_id     = NULL;
@@ -104,11 +105,13 @@ void heat_dissipation_par(int m, int n, float matrix[2][m][n], int np, float td,
   cl_uint ret_num_platforms;
   cl_int ret;
 
-  // memoire pour les matrices utilisés
-  cl_mem d_u0;
-  cl_mem d_u1;
-
   char string[MEM_SIZE];
+
+  unsigned int mem_size_A = sizeof(float) * 2 * m * n;
+  float* h_matrix = (float*) malloc(mem_size_A);
+  // h_matrix = memcpy(h_matrix, *matrix, mem_size_A);
+
+  cl_mem d_matrix;
 
   FILE *fp;
   char *source_str;
@@ -135,32 +138,67 @@ void heat_dissipation_par(int m, int n, float matrix[2][m][n], int np, float td,
   command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 
   // Create Memory Buffer
-  memobj = clCreateBuffer(context, CL_MEM_READ_WRITE,MEM_SIZE * sizeof(char), NULL, &ret);
+  d_matrix = clCreateBuffer(
+    context,
+    CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
+    mem_size_A,
+    h_matrix,
+    &ret
+  );
+
+  if (!d_matrix)
+  {
+    printf("Error: Failed to allocate device memory!\n");
+    exit(1);
+  }
 
   // Create Kernel Program from the source
   program = clCreateProgramWithSource(
     context,
     1,
     (const char **)&source_str,
-    (const size_t *)&source_size, &ret
+    (const size_t *)&source_size,
+    &ret
   );
 
   // Build Kernel Program
   ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+  if (ret != CL_SUCCESS)
+  {
+    size_t len;
+    char buffer[2048];
+    printf("Error: Failed to build program executable!\n");
+    clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer),
+    buffer, &len);
+
+    printf("%s\n", buffer);
+    exit(1);
+  }
 
   // Create OpenCL Kernel
   kernel = clCreateKernel(program, "heat_dissipation", &ret);
 
   // Set OpenCL Kernel Parameters
-  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&memobj);
+  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_matrix);
+  ret |= clSetKernelArg(kernel, 1, sizeof(int), (void *)&m);
+  ret |= clSetKernelArg(kernel, 2, sizeof(int), (void *)&n);
+  ret |= clSetKernelArg(kernel, 3, sizeof(int), (void *)&np);
+  ret |= clSetKernelArg(kernel, 4, sizeof(float), (void *)&td);
+  ret |= clSetKernelArg(kernel, 5, sizeof(float), (void *)&h);
+
+  if (ret != CL_SUCCESS)
+  {
+    printf("Error: Failed to set kernel arguments! %d\n", ret);
+    exit(1);
+  }
 
   // Execute OpenCL Kernel
   ret = clEnqueueTask(command_queue, kernel, 0, NULL,NULL);
 
   // Copy results from the memory buffer
   ret = clEnqueueReadBuffer(
-    command_queue, memobj, CL_TRUE, 0,
-    MEM_SIZE * sizeof(char),string, 0, NULL, NULL
+    command_queue, d_matrix, CL_TRUE, 0,
+    MEM_SIZE * sizeof(char), string, 0, NULL, NULL
   );
 
   // Display Result
@@ -171,7 +209,7 @@ void heat_dissipation_par(int m, int n, float matrix[2][m][n], int np, float td,
   ret = clFinish(command_queue);
   ret = clReleaseKernel(kernel);
   ret = clReleaseProgram(program);
-  ret = clReleaseMemObject(memobj);
+  ret = clReleaseMemObject(d_matrix);
   ret = clReleaseCommandQueue(command_queue);
   ret = clReleaseContext(context);
 
--- main.cl
+++ main.cl
@@ -1,27 +1,11 @@
-__kernel void hello(__global char* string)
+__kernel void heat_dissipation(
+  __global float* matrix,
+  int m,
+  int n,
+  int np,
+  float td,
+  float h)
 {
-  string[0] = 'H';
-  string[1] = 'e';
-  string[2] = 'l';
-  string[3] = 'l';
-  string[4] = 'o';
-  string[5] = ',';
-  string[6] = ' ';
-  string[7] = 'W';
-  string[8] = 'o';
-  string[9] = 'r';
-  string[10] = 'l';
-  string[11] = 'd';
-  string[12] = '!';
-  string[13] = '\0';
+  // int index = x + y * sizeX + z * sizeX * sizeY;
+  printf("matrix=%.2f, m=%d, n=%d np=%d, td=%.f, h=%.f\n", &matrix[15], m, n, np, td, h);
 }
-//
-// __kernel void heat_dissipation(
-//   float* u0,
-//   float* u1,
-//   __global int np,
-//   __global float td,
-//   __global float h)
-// {
-//
-// }
